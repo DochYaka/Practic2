91ff883e38e0353f8bea5f023ef6df734963db89
DochYaka <85236015+DochYaka@users.noreply.github.com>
2025-10-04 02:34:58 +0700

    Plant.cs

--- Plant.cs
+++ Plant.cs
@@ -7,24 +7,29 @@ public class Plant
     public string kind { get; private set; }
     public int age { get; private set; }
     public int water { get; set; }
+    public int height { get; private set; }
 
     public Plant()
     {
         kind = "Plant";
         age = 1;
         water = 0;
+        height = 1;
     }
 
-    public Plant(string kind, int age, int water)
+    public Plant(string kind, int age, int water, int height)
     {
         this.kind = string.IsNullOrWhiteSpace(kind) ? "Plant" : kind;
         this.age = (age < 1 || age > 1000) ? 1 : age;
         this.water = (water < 0 || water > 1) ? 0 : water;
+        this.height = (height < 1 || height > 15) ? 1 : height;
     }
 
     public virtual void Print()
     {
         Console.WriteLine($"\nВид : {kind}");
         Console.WriteLine($"Возраст : {age} г.");
+        Console.WriteLine($"Рост : {height} м");
+        Console.WriteLine($"Полито растение? {(water == 1 ? "Да" : "Нет")}");
     }
 }
