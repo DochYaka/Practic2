98c7bc69c6bc5536e14905a5d7b9377f17aac317
DochYaka <85236015+DochYaka@users.noreply.github.com>
2025-10-04 02:35:17 +0700

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// Program
+// Program
 using System;
 using System.Collections.Generic;
 
@@ -28,9 +28,9 @@ class Program
                         string type = InputFunc.ReadString("Введите растение: ");
                         int height = InputFunc.ReadInt("Введите рост растения (м): ", 1, 16);
                         int age = InputFunc.ReadInt("Введите возраст растения (г): ", 1, 1000);
-                        int water = InputFunc.ReadInt("Полито растение? ", 0, 1);
+                        int water = InputFunc.ReadInt("Полито растение? (0-Нет 1-Да): ", 0, 1);
                         
-                        garden.AddPlant(new Plant(type, age, water));
+                        garden.AddPlant(new Plant(type, age, water, height));
                         break;
                     }
 
@@ -43,7 +43,7 @@ class Program
 
                         int height = InputFunc.ReadInt("Введите рост дерева (м): ", 1, 15);
                         int age = InputFunc.ReadInt("Введите возраст дерева (г): ", 1, 1000);
-                        int water = InputFunc.ReadInt("Полито дерево? ", 0, 1);
+                        int water = InputFunc.ReadInt("Полито дерево? (0-Нет 1-Да): ", 0, 1);
 
                         Tree newTree = new Tree(type, sort, height, age, water);
                         garden.AddPlant(newTree);
@@ -56,10 +56,30 @@ class Program
                     {
                         string type = InputFunc.ReadString("Введите вид куста: ");
                         int age = InputFunc.ReadInt("Введите возраст куста (г): ", 1, 1000);
-                        int grow = InputFunc.ReadInt("Введите куста в процентах (г): ", 1, 1000);
-                        int water = InputFunc.ReadInt("Полит куст? ", 0, 1);
+                        int grow = InputFunc.ReadInt("Введите рост куста в процентах: ", 1, 1000);
+                        int water = InputFunc.ReadInt("Полит куст? (0 - нет, 1 - да): ", 0, 1);
 
-                        garden.AddPlant(new Shrub(type, age, grow, water));
+                        Shrub newShrub = new Shrub(type, age, grow, water);
+                        garden.AddPlant(newShrub);
+
+                        Console.WriteLine("\nВаш выбор:");
+                        Console.WriteLine("1 - Вырастить куст");
+                        Console.WriteLine("2 - Подрезать куст (-10%)");
+                        Console.WriteLine("0 - Назад в меню");
+
+                        int shrubChoice = InputFunc.ReadInt("Введите номер пункта: ", 0, 2);
+
+                        switch (shrubChoice)
+                        {
+                            case 1:
+                                newShrub.Grow();
+                                break;
+                            case 2:
+                                newShrub = newShrub - 10;
+                                Console.WriteLine("Куст подрезан на 10%.");
+                                break;
+                        }
+
                         break;
                     }
 
@@ -72,49 +92,49 @@ class Program
                 case 5:
                     {
                         garden.ShowTrees();
-                        
-                        if (garden.trees.Count != 0)
+                        List<Tree> treesInGarden = garden.GetTrees();
+
+                        if (treesInGarden.Count != 0)
                         {
                             Console.WriteLine("\nВаш выбор:");
                             Console.WriteLine("1 - Собрать урожай ");
-                            Console.WriteLine("2 - Срубить дерево (срезать дерево) ");
-                            Console.WriteLine("3 - Полить дерево (позаботиться) ");
+                            Console.WriteLine("2 - Срубить дерево ");
+                            Console.WriteLine("3 - Полить дерево ");
                             Console.WriteLine("4 - Потрогать под вдохновляющую музыку ");
                             Console.WriteLine("0 - Выход");
 
-                            int choice2 = InputFunc.ReadInt("Введите номер пункта: ", 0, 4);
+                            int choice2 = InputFunc.ReadInt("Введите номер пункта: \n", 0, 4);
 
                             switch (choice2)
                             {
                                 case 1:
                                     {
-                                        for (int i = 0; i < garden.trees.Count; i++) garden.trees[i].harvest();
-                                        Console.WriteLine("Урожай собран");
+                                        foreach (var tree in treesInGarden) tree.harvest();
                                         break;
                                     }
                                 case 2:
                                     {
-                                        int index = InputFunc.ReadInt("Какое дерево срубить? ", 1, garden.trees.Count);
-                                        garden.trees[index - 1].cut(garden, index);
+                                        int index = InputFunc.ReadInt("Какое дерево срубить? ", 1, treesInGarden.Count);
+                                        treesInGarden[index - 1].cut(garden, index);
                                         break;
                                     }
                                 case 3:
                                     {
-                                        int index = InputFunc.ReadInt("Какое дерево полить? ", 1, garden.trees.Count);
-                                        garden.trees[index - 1].care(garden, index);
+                                        int index = InputFunc.ReadInt("Какое дерево полить? ", 1, treesInGarden.Count);
+                                        treesInGarden[index - 1].care(garden, index);
                                         break;
                                     }
                                 case 4:
                                     {
-                                        garden.trees[0].touch();
+                                        treesInGarden[0].touch();
                                         break;
                                     }
                             }
                         }
 
                         break;
-                            
                     }
+
 
                 case 0:
                     return;
